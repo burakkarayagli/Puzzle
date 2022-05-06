7ff3036bb9c7da13c4175c689ef28a06067c213d
Hazewiooo <hazewiooo@gmail.com>
2022-05-07 01:09:02 +0300

    Controller silindi

--- src/main/java/com/example/puzzlegame/Main.java
+++ src/main/java/com/example/puzzlegame/Main.java
@@ -3,15 +3,18 @@ package com.example.puzzlegame;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        BorderPane borderPane = new BorderPane();
+
+        Scene scene = new Scene(borderPane,500,500);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
